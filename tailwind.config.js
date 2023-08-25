b6532be95c148e2ee76fcc60fa75ce4d738691de
Tobias <samu1493@stud.kea.dk>
2023-08-25 16:49:21 +0200

    lob folder added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ export default {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
     extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       colors: {
         primary: "#293144",
         secondary: "#078a1d",
